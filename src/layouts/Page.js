55f1f6c96d73f792287927f8af1bf4ec1b96683e
rafaelmaiach <rafael.chieregatto@hotmail.com>
2020-08-09 21:51:27 -0300

    feat(components): create Header

--- src/layouts/Page.js
+++ src/layouts/Page.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Header from '@/components/Header';
+
 import '@/styles/bulma.scss';
 
 const Page = ({ children }) => (
 	<div>
+		<Header />
 		<main>
 			{children}
 		</main>
