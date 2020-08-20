0ebe04389a9d971cc9fe4c85d8457adad486e102
rafaelmaiach <rafael.chieregatto@hotmail.com>
2020-08-20 19:04:21 -0300

    chore: refactor some styles on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import { Link } from 'gatsby';
+
 import Page from '@/layouts/Page';
-import ImagemMapBrasil from '../assets/images/components/mapa-brasil.png';
+
+import ImagemMapBrasil from 'assets/images/components/mapa-brasil.png';
 
 import './index.scss';
+
 const Home = () => (
 	<Page>
-		<div className="sobre">
-			<img src={ImagemMapBrasil} className="mapa-brasil"/>
-			<span className="txt-sobre">A <span class="title-fauna">FAUNA BRASILEIRA</span> tem como objetivo deixar acessível os dados sobre animais ameaçados de extinção e os que já foram extintos, assim como
-				a justificativa para o ocorrido e oque pode ser feito para ajudar na preservação das espécies ameaçadas.
-				<button className="btn-acessar-lista">Acessar Lista</button>
-			</span>
+		<div className="flex justify-center items-center flex-col lg:flex-row p-8">
+			<img src={ImagemMapBrasil} className="mapa-brasil" alt="Brazil map with some animals over it" />
+			<div className="flex flex-col items-start mt-8">
+				<span className="text-lg lg:text-2xl lg:p-10">
+					A <b>FAUNA BRASILEIRA</b> tem como objetivo deixar acessível os dados sobre os animais ameaçados de extinção e os que já foram extintos, assim como
+					a justificativa para o ocorrido e o que pode ser feito para ajudar na preservação das espécies ameaçadas.
+				</span>
+				<div className="lg:p-10 flex mt-4">
+					<Link to="/fauna-ameacada/" className="button bg-green-500 text-yellow-400 has-text-weight-bold border-none mr-8">
+						Ver Fauna Ameaçada
+					</Link>
+					<Link to="/fauna-extinta/" className="button bg-green-500 text-yellow-400 has-text-weight-bold border-none">
+						Ver Fauna Extinta
+					</Link>
+				</div>
+			</div>
 		</div>
 	</Page>
 );
